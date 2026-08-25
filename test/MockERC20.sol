af0b1ef3a5a2615481e457834cbd5235257487d2
Arventis Labs <contact.arventislabs@gmail.com>
2026-08-25 06:26:18 -0700

    chore: sync all files and clean git tree for production ready state

--- test/MockERC20.sol
+++ test/MockERC20.sol
@@ -7,6 +7,6 @@ contract MockERC20 is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {}
 
     function mint(address to, uint256 amount) external {
-        _mint(to, amount);
+        _mint(to, amount); 
     }
 }
